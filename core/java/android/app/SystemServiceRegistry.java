5b0fa58b88a1c4dcc6502c9ff94047c4befa52db
Erfan Abdi <erfangplus@gmail.com>
2022-07-21 18:02:27 +0430

    base: Introduce LiveDisplay from Lineage
    Change-Id: Ie3a61a67edfc4f4568fa2341791fa9fa5263f90d
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>
    Signed-off-by: Jis G Jacob <studiokeys@blissroms.org>
    Signed-off-by: HeroBuxx <me@herobuxx.me>

--- core/java/android/app/SystemServiceRegistry.java
+++ core/java/android/app/SystemServiceRegistry.java
@@ -266,6 +266,10 @@ import com.android.internal.os.IDropBoxManagerService;
 import com.android.internal.policy.PhoneLayoutInflater;
 import com.android.internal.util.Preconditions;
 
+import com.android.internal.halcyon.app.LineageContextConstants;
+import com.android.internal.halcyon.hardware.LiveDisplayManager;
+import com.android.internal.halcyon.hardware.ILiveDisplayService;
+
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
@@ -967,6 +971,19 @@ public final class SystemServiceRegistry {
                         final IAuthService service =
                                 IAuthService.Stub.asInterface(binder);
                         return new BiometricManager(ctx.getOuterContext(), service);
+                    }
+                });
+
+        registerService(LineageContextConstants.LINEAGE_LIVEDISPLAY_SERVICE, LiveDisplayManager.class,
+                new CachedServiceFetcher<LiveDisplayManager>() {
+                    @Override
+                    public LiveDisplayManager createService(ContextImpl ctx)
+                            throws ServiceNotFoundException {
+                        final IBinder binder =
+                                ServiceManager.getServiceOrThrow(LineageContextConstants.LINEAGE_LIVEDISPLAY_SERVICE);
+                        final ILiveDisplayService service =
+                                ILiveDisplayService.Stub.asInterface(binder);
+                        return new LiveDisplayManager(service);
                     }
                 });
 
--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -240,6 +240,7 @@ import com.android.server.wm.ActivityTaskManagerService;
 import com.android.server.wm.WindowManagerGlobalLock;
 import com.android.server.wm.WindowManagerService;
 import com.android.server.halcyon.ParallelSpaceManagerService;
+import com.android.server.halcyon.display.LiveDisplayService;
 
 import dalvik.system.VMRuntime;
 
@@ -2717,6 +2718,10 @@ public final class SystemServer implements Dumpable {
             // LineageHardware
             t.traceBegin("StartLineageHardwareService");
             mSystemServiceManager.startService(LineageHardwareService.class);
+            t.traceEnd();
+
+            t.traceBegin("StartLiveDisplayService");
+            mSystemServiceManager.startService(LiveDisplayService.class);
             t.traceEnd();
         }
 
