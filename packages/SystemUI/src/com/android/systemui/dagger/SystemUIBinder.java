PixysOS/frameworks_base

SystemUI: Implement ambient music support
[aswin7469] : Rebranded, fixed for 14 QPR2 and moved directly to fw base

Co-authored-by: Anay Wadhera <anay1018@gmail.com>
Change-Id: If97543ef26aa1e747b637ffc9b128eaa1c64ac53
Signed-off-by: aswin7469 <aswinas@pixysos.com>

packages/SystemUI/src/com/android/systemui/dagger/SystemUIBinder.java
@@ -16,11 +16,18 @@
 
 package com.android.systemui.dagger;
 
+import com.android.systemui.CoreStartable;
+
 import com.android.systemui.keyguard.dagger.KeyguardModule;
 import com.android.systemui.recents.RecentsModule;
 import com.android.systemui.statusbar.dagger.CentralSurfacesModule;
 
+import com.google.android.systemui.GoogleServices;
+
+import dagger.Binds;
 import dagger.Module;
+import dagger.multibindings.ClassKey;
+import dagger.multibindings.IntoMap;
 
 /**
  * SystemUI objects that are injectable should go here.
@@ -31,4 +38,12 @@ import dagger.Module;
         KeyguardModule.class,
 })
 public abstract class SystemUIBinder {
+
+    /**
+     * Inject into GoogleServices.
+     */
+    @Binds
+    @IntoMap
+    @ClassKey(GoogleServices.class)
+    public abstract CoreStartable bindStatixServices(GoogleServices sysui);
 }

xyzuniverse/halcyon_frameworks_base

Signed-off-by: HeroBuxx <me@herobuxx.me>

packages/SystemUI/src/com/android/systemui/dagger/SystemUIBinder.java
@@ -16,11 +16,18 @@
 
 package com.android.systemui.dagger;
 
+import com.android.systemui.CoreStartable;
+
 import com.android.systemui.keyguard.dagger.KeyguardModule;
 import com.android.systemui.recents.RecentsModule;
 import com.android.systemui.statusbar.dagger.CentralSurfacesModule;
 
+import com.google.android.systemui.GoogleServices;
+
+import dagger.Binds;
 import dagger.Module;
+import dagger.multibindings.ClassKey;
+import dagger.multibindings.IntoMap;
 
 /**
  * SystemUI objects that are injectable should go here.
@@ -31,4 +38,12 @@ import dagger.Module;
         KeyguardModule.class,
 })
 public abstract class SystemUIBinder {
+
+    /**
+     * Inject into GoogleServices.
+     */
+    @Binds
+    @IntoMap
+    @ClassKey(GoogleServices.class)
+    public abstract CoreStartable bindGoogleServices(GoogleServices sysui);
 }